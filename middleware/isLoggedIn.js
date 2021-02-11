35e9227c27702da4abe6eaa5ae12648e331ced39
nvalerio12 <nelson.j.valerio@gmail.com>
2021-02-11 17:06:17 -0500

    added middleware

---
+++ middleware/isLoggedIn.js
@@ -0,0 +1,9 @@
+function idLoggedIn(req, res, next) {
+    if (!req.user) {
+        req.flash('error', 'You must be signed in to access page');
+    } else {
+        next();
+    }
+}
+
+module.exports = isLoggedIn;
--- server.js
+++ server.js
@@ -2,14 +2,45 @@ require('dotenv').config();
 
 const express = require('express');
 const layouts = require('express-ejs-layouts');
-const app = express();
+const session = require('express-session');
+// import passpot Config, comeback later
+const flash = require('connect-flash');
+const passport = require('passport');
 
+
+const app = express();
 app.set('view engine', 'ejs');
+const SECRET_SESSION = process.env.SECRET_SESSION
 
 app.use(require('morgan')('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'));
 app.use(layouts);
+
+// secret: What we actually will be giving the user on our site as a session cookie
+// resave: Save the session even if it's modified, make this false
+// saveUninitialized: If we have a new session, we save it, therefore making that true
+const sessionObject = {
+  secret: SECRET_SESSION,
+  resave: false,
+  saveUninitialized: true
+}
+app.use(session(sessionObject));
+
+app.use(session(sessionObject));
+
+//Passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Flash
+app.use(flash());
+app.use((req, res, next) => {
+  console.log(res.locals);
+  res.locals.alerts = req.flash();
+  res.locals.currentUser = req.user;
+  next();
+}) 
 
 // Routes 
 app.use('/auth', require('./routes/auth'));
